649a13d8868958900ecf00b7eafbf76a8b4e40e6
eugène <eugenevalty@gmail.com>
2020-12-14 16:10:05 +0100

    les correctionz

--- apprentissage.ml
+++ apprentissage.ml
@@ -3,14 +3,14 @@
    recommandée. Le code que vous devez écrire prend en entrée deux
    listes de mots I et E, lues à partir d'un fichier passé en argument
    et renvoie sur la sortie standard le code SMT-LIB à faire passer
-   dans une solveur SMT, comme Z3. 
+   dans une solveur SMT, comme Z3.
  *)
 
 open Printf
 
 (* ensembles de test : ensemble I *)
 let li = ["";"ab"]
-             
+
 (* ensembles de test : ensemble E *)
 let le = ["aa";"b"]
 
@@ -19,10 +19,10 @@ let le = ["aa";"b"]
 (* EXERCICE 1 : extraire  l'alphabet de l'automate.
    La fonction alphabet_from_list prend en entrée une liste de
    chaînes de caractères et retourne une liste triée de
-   caractères sans duplication. 
+   caractères sans duplication.
  *)
-(* explode : string -> char list 
-   prend une chaîne de caractères et renvoie la liste de ses caractères 
+(* explode : string -> char list
+   prend une chaîne de caractères et renvoie la liste de ses caractères
  *)
 let explode s =
   let rec exp i l =
@@ -39,7 +39,7 @@ let rec insert l x =
      else if x = h
      then t
      else h :: (insert t x)
-  
+
 (*ajoute les caractères de l2 dans l1 s'ils n'y sont pas déjà *)
 let rec add l1 l2 =
   match l2 with
@@ -48,9 +48,9 @@ let rec add l1 l2 =
      if List.mem h l1
      then add l1 t
      else add (insert l1 h) t
-    
-(* alphabet_from_list : string list -> char list  
-   - prend en entrée une liste l de chaînes de caractères 
+
+(* alphabet_from_list : string list -> char list
+   - prend en entrée une liste l de chaînes de caractères
    - renvoie la liste triée et sans duplication de caractères
      apparaissant dans l
  *)
@@ -62,7 +62,7 @@ let alphabet_from_list l =
        alphabet t (add a (explode h))
   in
   alphabet l []
-  
+
 (* test *)
 (*let a = alphabet_from_list (li @ le)*)
 
@@ -73,7 +73,7 @@ let alphabet_from_list l =
    "(declare-datatypes () ((A a b) (T e ea eaa eab eb)))" *)
 
 (* prefixes : string -> string list
-   renvoie la liste des préfixes d'une chaîne de caractères 
+   renvoie la liste des préfixes d'une chaîne de caractères
    Nous allons ajouter à chaque préfixe le caractère 'e'.
    Par exemple, prefixes "aba" = ["e"; "ea"; "eab"; "eaba"] *)
 let rec prefixes s =
@@ -81,15 +81,15 @@ let rec prefixes s =
     "e" ^ String.sub s 0 i
   in
   List.init (String.length s + 1) f
-  
+
 (* prefixes_of_list : string list -> string list
-   renvoie la liste triée et sans doublons des préfixes des chaînes 
+   renvoie la liste triée et sans doublons des préfixes des chaînes
    de caractères d'une liste donnée *)
 let prefixes_of_list l =
   add [] l
 
 (* declare_types_alphabet : char list -> string
-   prend une liste de caractères [c_1; ...; c_n] et renvoie une chaîne 
+   prend une liste de caractères [c_1; ...; c_n] et renvoie une chaîne
    de la forme "(A c_1... c_n)" *)
 let declare_types_alphabet cl =
   let rec aux l =
@@ -103,22 +103,22 @@ let rec declare_types_t l =
   match l with
   | [] -> ")"
   | h :: t -> " " ^ h ^ (declare_types_t t)
-            
-(* declare_types_trie : string list -> string 
-   prend une liste l de chaînes de caractères et 
-   renvoie une chaîne de la forme "(T es_1 ... es_n)" où 
-   s_1... s_n est une énumération de tous les 
+
+(* declare_types_trie : string list -> string
+   prend une liste l de chaînes de caractères et
+   renvoie une chaîne de la forme "(T es_1 ... es_n)" où
+   s_1... s_n est une énumération de tous les
    prefixes des chaînes apparaissant dans l *)
 let declare_types_trie l =
   "(T" ^ (declare_types_t (prefixes_of_list (List.flatten (List.map prefixes l))))
 
-(* declare_types : string list -> char list -> string *)  
+(* declare_types : string list -> char list -> string *)
 let declare_types l cl =
   "(declare-datatypes () (" ^ (declare_types_alphabet cl) ^ " " ^ (declare_types_trie l) ^ "))";;
-  
+
 (* test *)
 (*Printf.printf "%s" (declare_types (li @ le) a) *)
-  
+
 
 (* ======================================================================= *)
 (* EXERCICE 3 : définir une chaîne de caractères pour les définitions
@@ -146,23 +146,23 @@ let define_sorts_and_functions  =
 (and (>= (f x) 0) (< (f x) n))))
 
 (assert (= 0 (f e)))"
-  
+
 (* ======================================================================= *)
 (* EXERCICE 4 : contraintes sur les transitions
-   La fonction assert_transition_constraints prend en entrée une trie 
-   et retourne une chaîne qui modélise les contraintes sur les transitions 
+   La fonction assert_transition_constraints prend en entrée une trie
+   et retourne une chaîne qui modélise les contraintes sur les transitions
    de l'automate décrites par la formule (56). *)
-  
+
 (* eq_trans_constr : string -> char -> string
-   renvoie une chaîne de caractères qui correspond à une formule atomique pour 
+   renvoie une chaîne de caractères qui correspond à une formule atomique pour
    la transition étiquetée avec 'a' à partir de l'état s
-   Par exemple, pour s = "abc" et  c = 'd' on a 
+   Par exemple, pour s = "abc" et  c = 'd' on a
    eq_trans_constr outputs "(= (f abcd)  (delta (f abc)  d))" *)
 let eq_trans_constr s a =
    "           (= (f " ^ s ^ Char.escaped a ^ ") (delta (f " ^ s ^ ") " ^ Char.escaped a ^"))"
 
 (* list_transition_constraints : string list -> string list
-   prend une liste de chaînes de caractères et génère une liste 
+   prend une liste de chaînes de caractères et génère une liste
    de formules atomiques ou de chaînes vides comme suit
    - pour une chaîne non vide de la forme sa on obtient
      une chaine correspondant à l'équation f(sa) = delta (fs) a
@@ -176,13 +176,13 @@ let rec list_transition_constraints l =
      else
        eq_trans_constr (String.sub h 0 (String.length h - 1)) (String.get h (String.length h - 1))
        :: (list_transition_constraints t)
-    
+
 (* assert_transition_constraints : string list -> string
-   prend en entrée une liste de mots et renvoie une chaîne qui modélise 
-   les contraintes sur les transitions de l'automate décrit par la 
+   prend en entrée une liste de mots et renvoie une chaîne qui modélise
+   les contraintes sur les transitions de l'automate décrit par la
    formule (56).
    Par exemple, pour la liste [""; "ab"; "aa"; "b"] on obtient la chaîne
-   "(assert (and 
+   "(assert (and
                (= (f ea)  (delta (f e)  a))
                (= (f eaa)  (delta (f ea)  a))
                (= (f eab)  (delta (f ea)  b))
@@ -193,10 +193,10 @@ let rec lisse l =
     match l with
     | [] -> ""
     | h :: t -> h ^ "\n" ^ (lisse t)
-    
+
 let assert_transition_constraints l =
   "(assert (and\n"
-  ^ lisse (list_transition_constraints (prefixes_of_list (List.flatten (List.map prefixes (li @ le)))))
+  ^ lisse (list_transition_constraints (prefixes_of_list (List.flatten (List.map prefixes l))))
   ^ "))";;
 
 (* test *)
@@ -208,15 +208,15 @@ let assert_transition_constraints l =
    une chaîne de caractères qui modélise les contraintes sur les états
    acceptants décrites par la formule (57). *)
 
-(* eq_accept : string -> string 
-   - prend une chaîne de caractères s et renvoie une chaîne de caractères 
+(* eq_accept : string -> string
+   - prend une chaîne de caractères s et renvoie une chaîne de caractères
    qui modélise l'appartenance de s à l'ensemble final des états acceptants *)
 let eq_accept s =
   "           (final (f " ^ s ^ "))"
 
-(* eq_non_accept : string -> string 
-   - prend une chaîne de caractères s et renvoie une chaîne de caractères 
-   qui modélise la non-appartenance de s à l'ensemble final des états acceptants 
+(* eq_non_accept : string -> string
+   - prend une chaîne de caractères s et renvoie une chaîne de caractères
+   qui modélise la non-appartenance de s à l'ensemble final des états acceptants
  *)
 let eq_non_accept s =
   "           (not(final (f " ^ s ^ ")))"
@@ -224,7 +224,7 @@ let eq_non_accept s =
 (* assert_acceptance : string list -> string list > string
    prend deux listes de chaînes de caractères, li et le, et renvoie une
    chaine qui modélise les contraintes sur les états acceptants
-   décrites par la formule (52). 
+   décrites par la formule (52).
    Les mots dans li sont acceptés et les mots dans le ne le sont pas. *)
 let assert_acceptance li le  =
   let adde a =
@@ -233,22 +233,22 @@ let assert_acceptance li le  =
   "(assert (and \n"
   ^ lisse (List.map eq_accept (List.map adde li))
   ^ lisse (List.map eq_non_accept (List.map adde le))
-  ^ "))";;
-  
+  ^ "))\n";;
+
 (* test *)
 (*Printf.printf "%s" (assert_acceptance li le)*)
-  
+
 (* ======================================================================= *)
 (* EXERCICE 6 :
    La fonction smt_code prend en entrée deux listes de mots
-   et retourne une chaîne de caractères qui donne l'implémentation 
+   et retourne une chaîne de caractères qui donne l'implémentation
    en SMT-LIB. *)
 
-(* smt_code : string list -> string list -> string 
-   prend deux listes de chaînes de caractères, li et le, et renvoie une chaîne 
+(* smt_code : string list -> string list -> string
+   prend deux listes de chaînes de caractères, li et le, et renvoie une chaîne
    de caractères qui donne l'implémentation en SMT-LIB.
-   Les mots dans li sont acceptés et les mots dans le ne le sont pas. 
-   Pour vérifier votre algorithme, vous pouvez essayer le code SMT-LIB 
+   Les mots dans li sont acceptés et les mots dans le ne le sont pas.
+   Pour vérifier votre algorithme, vous pouvez essayer le code SMT-LIB
    que vous obtenez dans le solveur Z3: https://rise4fun.com/z3 *)
 let smt_code li le =
   declare_types (li @ le) (alphabet_from_list (li @ le))
@@ -258,11 +258,11 @@ let smt_code li le =
   ^ assert_transition_constraints (li @ le)
   ^ "\n\n"
   ^ assert_acceptance li le
-  ^ "\n(check-sat-using (then qe smt))
-(get-value ((delta 0 a) (delta 0 b) (delta 1 a) (delta 1 b) (final 0) (final 1)))
+  ^ "\n(check-sat-using (then qe smt))\n
+(get-model)\n
 (exit)\n"
   ;;
-  
+
 (* test *)
   (*Printf.printf "%s" (smt_code li le)*)
 
@@ -274,7 +274,7 @@ let smt_code li le =
 let input_line_opt ic =
   try Some (input_line ic)
   with End_of_file -> None
-                    
+
 let read_lines ic =
   let rec aux acc =
     match input_line_opt ic with
@@ -282,7 +282,7 @@ let read_lines ic =
     | None -> (List.rev acc)
   in
   aux []
-  
+
 let lines_of_file filename =
   let ic = open_in filename in
   let lines = read_lines ic in
@@ -293,7 +293,7 @@ let read_lists_from_file (filename: string): ((string list) * (string list))  =
   let lines = lines_of_file filename in
   (String.split_on_char ' ' (List.nth lines 0),
    String.split_on_char ' ' (List.nth lines 1))
-  
+
 let () =
   let (li,le) = (read_lists_from_file Sys.argv.(1)) in
   Printf.printf "%s" (smt_code li le)
